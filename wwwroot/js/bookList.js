b9ee7e83e5266bed0d187eeb9214a5281d4dfd48
kalanatebel97 <kalanamtebel97@gmail.com>
2020-03-27 22:58:58 +0530

    Delete toaster message

--- wwwroot/js/bookList.js
+++ wwwroot/js/bookList.js
@@ -1,4 +1,6 @@
-﻿var dataTable;
+﻿
+
+var dataTable;
 
 $(document).ready(function () {
 
@@ -26,7 +28,8 @@ function loadDataTable() {
                                     Edit 
                                 </a>
                                 &nbsp;
-                                <a class = "btn btn-danger text-white" style = "cursor:pointer; width:70px";>
+                                <a class = "btn btn-danger text-white" style = "cursor:pointer; width:70px;"
+                                    onClick=Delete('api/book?id='+${data})>
                                     Delete 
                                 </a>
                             </div>`;    
@@ -38,5 +41,32 @@ function loadDataTable() {
             "emptyTable": "No data found"
         },
         "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+
+        title: "Are you sure?",
+        text: "Once you deleted,you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+
+                    }
+                }
+            });
+        }
     });
 }
