d4d48e8aae3bb63c614748d75f016937689fb3fc
kalanatebel97 <kalanamtebel97@gmail.com>
2020-03-28 09:51:26 +0530

    Upsert Razor page changes

--- wwwroot/js/bookList.js
+++ wwwroot/js/bookList.js
@@ -24,7 +24,7 @@ function loadDataTable() {
 
                     return `<div class = "text-center">
 
-                                <a href = "/BookList/Edit?id=${data}" class = "btn btn-success text-white" style = "cursor:pointer; width:70px";>
+                                <a href = "/BookList/Upsert?id=${data}" class = "btn btn-success text-white" style = "cursor:pointer; width:70px";>
                                     Edit 
                                 </a>
                                 &nbsp;
